79fcc8176e2d785a902f924d2ed23b53fdfc8821
rabiulislam13579 <rabiul120158@gmail.com>
2023-05-21 18:56:09 +0600

    post

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db('toyManager').collection('toys')
+        const bookingCollection = client.db('toyBooking').collection('bookings')
+
+        app.post('/toys', async (req, res) => {
+            const data = req.body;
+
+            const result = await toyCollection.insertOne(data)
+            res.send(result)
+        })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
