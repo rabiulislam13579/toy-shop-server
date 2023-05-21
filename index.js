6ba4a37e257498304b9be1948cc3cbcaeb256569
rabiulislam13579 <rabiul120158@gmail.com>
2023-05-21 19:05:57 +0600

    final

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app=express();
 const cors = require('cors');
 require('dotenv').config()
-const port =process.env.PORT ||5000;
+const app = express();
+const port = process.env.PORT || 5000;
 
-//middleware
 app.use(express.json());
 app.use(cors())
+// const coresConfig ={
+//     origin: '',
+//     Credential: true,
+//     methods:['GET','POST', 'PUT', 'DELETE']
+// }
+// app.use(cors(coresConfig))
+// app.options("",cors(coresConfig))
 
-app.get('/',(req,res)=>{
-    res.send('toy server is running')
-})
 
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wree0kk.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kfqp20s.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const toyCollection = client.db('toyManager').collection('toys')
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+
+        app.get('/', (req, res) => {
+            res.send("TOY Request Called")
+        })
+
+        const toyCollection = client.db('toyManager').collection('toys')
         const bookingCollection = client.db('toyBooking').collection('bookings')
 
         app.post('/toys', async (req, res) => {
@@ -39,6 +44,11 @@ async function run() {
             const result = await toyCollection.insertOne(data)
             res.send(result)
         })
+
+
+        
+
+
 
         app.get('/toys', async (req, res) => {
             const toys = toyCollection.find()
@@ -63,7 +73,7 @@ async function run() {
             res.send(result)
         })
 
-         //=====find my toy======
+        //=====find my toy======
 
         //=====bookings=====
 
@@ -91,6 +101,9 @@ async function run() {
             const result = await bookingCollection.findOne(query);
             res.send(result)
         })
+
+
+
 
         app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
@@ -120,19 +133,22 @@ async function run() {
         })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
-
-app.listen(port,()=>{
-    console.log(`the toy server is running on port:${port}`)
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`);
 })
-
-
