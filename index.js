12eba43d733ae16b84edb737809c7bbfd2308ec8
rabiulislam13579 <rabiul120158@gmail.com>
2023-05-21 18:22:49 +0600

    middleware

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app=express();
+const cors = require('cors');
+require('dotenv').config()
 const port =process.env.PORT ||5000;
+
+//middleware
+app.use(express.json());
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('toy server is running')
